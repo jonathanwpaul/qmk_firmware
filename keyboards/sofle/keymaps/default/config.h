e471370f9a8ff46aa5073cdb4d94658faafd5872
Jonathan <paulwjonathan@gmail.com>
2026-04-22 11:53:53 -0400

    sync with v2 of four84

--- keyboards/sofle/keymaps/default/config.h
+++ keyboards/sofle/keymaps/default/config.h
@@ -6,3 +6,19 @@
 #define TRI_LAYER_LOWER_LAYER 2
 #define TRI_LAYER_UPPER_LAYER 3
 #define TRI_LAYER_ADJUST_LAYER 4
+
+#define EE_HANDS
+
+/* clear default encoder def */
+#undef TAP_CODE_DELAY
+
+/* encoder support */
+#define TAP_CODE_DELAY 100
+
+#define SPLIT_WPM_ENABLE
+#define SPLIT_OLED_ENABLE
+
+#define POINTING_DEVICE_ROTATION_90
+#define SPLIT_POINTING_ENABLE
+#define POINTING_DEVICE_RIGHT
+#define POINTING_DEVICE_TASK_THROTTLE_MS 8
