49f39864a62dddcf55f8d11f56cccc257f3641bc
Jonathan <paulwjonathan@gmail.com>
2026-04-25 15:23:45 -0400

    fix binds

--- keyboards/sofle/keymaps/default/keymap.c
+++ keyboards/sofle/keymaps/default/keymap.c
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,    XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
 
-                   KC_LCTL, KC_LGUI, KC_LALT, KC_LOWER, KC_ENT,        KC_SPC,  KC_RAISE, KC_RALT, KC_RGUI, KC_RCTL
+                   KC_LCTL, KC_LALT, KC_LGUI, KC_LOWER, KC_ENT,        KC_SPC,  KC_RAISE, KC_RGUI, KC_RALT, KC_RCTL
     ),
     // clang-format on
 
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,                      KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,    XXXXXXX, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
 
-                   KC_LCTL, KC_LGUI, KC_LALT, KC_LOWER, KC_ENT,        KC_SPC,  KC_RAISE, KC_RALT, KC_RGUI, KC_RCTL
+                   KC_LCTL, KC_LALT, KC_LGUI, KC_LOWER, KC_ENT,        KC_SPC,  KC_RAISE, KC_RGUI, KC_RALT, KC_RCTL
     ),
     // clang-format on
 
